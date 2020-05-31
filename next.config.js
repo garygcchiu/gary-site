261ef8531d64622a332ccc6545e85840d35d3983
Gary Chiu <gary.gc.chiu@gmail.com>
2020-05-31 16:34:06 -0400

    web app manifest configuration for favicon

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const withImages = require('next-images');
 const GOOGLE_ANALYTICS_TRACKING_ID = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
 
 module.exports = withSass(withImages({
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.(webmanifest)$/i,
+            loader: ["file-loader", "app-manifest-loader"]
+        });
+        return config
+    },
     env: {
         GOOGLE_ANALYTICS_TRACKING_ID: GOOGLE_ANALYTICS_TRACKING_ID,
     },
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Head from 'next/head';
 import "../styles/main.scss";
 import favicon from '../public/favicon.ico';
+import appleTouchFavicon from '../public/apple-touch-icon.png';
+import manifest from '../public/site.webmanifest';
 import ReactGA from 'react-ga';
 
 const MyApp = ({Component, pageProps}) => {
@@ -11,6 +13,8 @@ const MyApp = ({Component, pageProps}) => {
     return <>
         <Head>
             <link rel="shortcut icon" href={favicon} />
+            <link rel="apple-touch-icon" sizes="180x180" href={appleTouchFavicon} />
+            <link rel="manifest" href={manifest} />
             <title>Gary Chiu</title>
         </Head>
         <Component {...pageProps} />
