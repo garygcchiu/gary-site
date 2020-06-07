2c9a63ef89fc4ef6c6b1cbd086c8aba3e39e2585
Gary Chiu <gary.gc.chiu@gmail.com>
2020-06-07 00:49:27 -0400

    shadows for the image sections

--- components/ImageSection.js
+++ components/ImageSection.js
@@ -2,7 +2,8 @@ const ImageSection = () => {
     return <section
         className={'image-section'}
     >
-        <div className={'image-section__overlay'}/>
+        <div className={'image-section__overlay image-section__overlay--top'}/>
+        <div className={'image-section__overlay image-section__overlay--bottom'}/>
     </section>
 };
 
