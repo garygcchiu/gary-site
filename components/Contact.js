a7e3f87613d3e6860254466506f635b4c4ea7646
Gary <gary.gc.chiu@gmail.com>
2020-09-19 13:11:16 -0400

    contact fades in too now

--- components/Contact.js
+++ components/Contact.js
@@ -1,18 +1,19 @@
 import Footer from './Footer';
+import FadeInDiv from './FadeInDiv';
 
 const Contact = () => {
     return <div className={'contact'}>
-        <div className={'contact__main'}>
+        <FadeInDiv className={'contact__main'} threshold={0.5}>
             <span className={'mr-2 contact__text'}>
                 Interested?
             </span>
             <a href="#mailgo" data-address="gary.gc.chiu" data-domain="gmail.com">
                 Contact me!
             </a>
-        </div>
-        <div className={'contact__footer'}>
+        </FadeInDiv>
+        <FadeInDiv className={'contact__footer'}>
             <Footer />
-        </div>
+        </FadeInDiv>
     </div>;
 };
 
