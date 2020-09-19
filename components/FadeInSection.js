7ff62306bd2cc58b2fb22729e250759281a21bb8
Gary <gary.gc.chiu@gmail.com>
2020-09-18 21:16:46 -0400

    sections now work properly with dark-theme

--- components/FadeInSection.js
+++ components/FadeInSection.js
@@ -1,22 +1,16 @@
 import { useInView } from 'react-intersection-observer';
 
-const FadeInSection = ({ children, className = '', fadeChildrenOnly = false }) => {
+const FadeInSection = ({ children, className = ''}) => {
     const [ref, inView] = useInView({
         threshold: 0.70,
         triggerOnce: true
     });
 
-    if (!fadeChildrenOnly){
-        return <section ref={ref} className={`fade-in-section ${inView ? 'fade-in-section--visible' : '' } ${className} section`}>
+    return <section className={className}>
+        <div ref={ref} className={`fade-in-section ${inView ? 'fade-in-section--visible' : '' }`}>
             { children }
-        </section>;
-    } else {
-        return <section className={className}>
-            <div ref={ref} className={`section fade-in-section ${inView ? 'fade-in-section--visible' : '' } section`}>
-                { children }
-            </div>
-        </section>;
-    }
+        </div>
+    </section>;
 };
 
 export default FadeInSection;
--- components/ImageSection.js
+++ components/ImageSection.js
@@ -1,8 +1,8 @@
 const ImageSection = () => {
-    return <section className={'image-section section'}>
+    return <div className={'image-section'}>
         <div className={'image-section__overlay image-section__overlay--top'}/>
         <div className={'image-section__overlay image-section__overlay--bottom'}/>
-    </section>
+    </div>
 };
 
 export default ImageSection;
