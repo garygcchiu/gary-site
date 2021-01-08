5cefcd89f96411640821200483869ae42e2f51da
Gary <gary.gc.chiu@gmail.com>
2021-01-08 18:59:37 -0500

    updated resume to 2021

--- components/Socials.js
+++ components/Socials.js
@@ -1,6 +1,6 @@
 import * as ReactGA from '../utils/react-ga';
 import IconLink from './IconLink';
-import resume from '../public/Gary_Chiu_Resume_2020.pdf';
+import resume from '../public/Gary_Chiu_Resume_2021.pdf';
 
 const handleResumeDownloaded = () => {
     ReactGA.sendEvent('Link', 'Downloaded Resume');
